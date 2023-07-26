ba2739688bc874fdad2feb686fd4ef1257f39824
chinmaysingh1 <70607148+chinmaysingh1@users.noreply.github.com>
2023-07-26 18:35:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|OperaMini/i.test(navigator.userAgent) ) {
-    document.location = "https://m.biocast.co";
-   }
-
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
     var x = document.getElementById("myLinks");
